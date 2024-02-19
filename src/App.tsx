9f425f5a88bc79ee853aca174e15c0e237937cd3
SedPC <lilshed3@gmail.com>
2024-02-18 20:05:55 -0800

    Add note functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { dummyData } from "./data/TodoData"
 import TodoItem from "./components/TodoItem"
 import { useState } from "react"
+import AddTodoForm from "./components/AddTodoForm";
 
 
 function App() {
@@ -15,10 +16,24 @@ function App() {
     ));
   }
 
+  function addTodo(input: string) {
+    setTodos((prevTodos) => ([
+      {
+        id: todos.length + 1,
+        title: input,
+        completed: false,
+      },
+      ...prevTodos
+    ]))
+
+    console.log(todos);
+  }
+
   return (
     <div className="flex justify-center">
       <main className="bg-gray-500 w-11/12 ">
         <h1 className='py-10 font-bold text-3xl text-center bg-gray-400'>TODO: List</h1>
+        <AddTodoForm onSubmit={addTodo}/>
         <div className="max-w-lg mx-auto p-5">
           <div className="space-y-5 p-5 bg-gray-400">
             {todos.map(Todo => (
---
+++ src/components/AddTodoForm.tsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+interface AddTodoFormProps {
+    onSubmit(title: String): void;
+}
+
+export default function AddTodoForm({onSubmit}: AddTodoFormProps) {
+
+    //"controlled" input form
+    //we use state to manage the input form value
+    const [input, setInput] = useState("");
+
+    function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+
+        if(!input.trim()) {
+            return;
+        }
+
+        onSubmit(input);
+        setInput("");
+    }
+
+    return (
+        <div className="m-5">
+            <form className="flex" onSubmit={handleSubmit}>
+                <input 
+                    placeholder="Enter your task..."
+                    className="rounded-l-md grow border-neutral-700 p-2"
+                    value={input}
+                    onChange={(e) => setInput(e.target.value)}
+                />
+                <button 
+                    type="submit" 
+                    className="w-16 rounded-r-md bg-slate-900 text-white"
+                >
+                    Add
+                </button>
+            </form>
+        </div>
+    );
+} 
